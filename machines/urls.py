786d7ac0f4268534573516442607a1b5bdf1af49
wszemart <wszeborska@wp.pl>
2023-08-20 20:05:31 +0200

    Added breadcrumb support to the 'machines' application and the ability to print the tool table to a CSV file.

--- machines/urls.py
+++ machines/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import MachineDetailView, MachineListView, home, MachineCreateView, MachineDeleteView, MachineUpdateView
-
-
+from .views import MachineDetailView, home, MachineCreateView, MachineDeleteView, MachineUpdateView, generate_csv
 urlpatterns = [
     path('', home, name='app-home'),
     path('machine/<int:pk>/', MachineDetailView.as_view(), name='machine-detail'),
     path('machine/new/', MachineCreateView.as_view(), name='machine-create'),
     path('machine/update/<int:pk>', MachineUpdateView.as_view(), name='machine-update'),
-    path('machine/delete/<int:pk>', MachineDeleteView.as_view(), name='machine-delete')
+    path('machine/delete/<int:pk>', MachineDeleteView.as_view(), name='machine-delete'),
+    path('tools/generate-csv/', generate_csv, name='generate-csv'),
+    # path('tools/generate-pdf/', generate_pdf, name='generate-pdf'),
 ]
 
--- machines/views.py
+++ machines/views.py
@@ -6,8 +6,35 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import MachineForm
 from .models import Machine
 from django.contrib.auth.decorators import login_required
+import pandas as pd
+from django.http import HttpResponse
+# from weasyprint import HTML
+from django.template.loader import render_to_string
 from django.shortcuts import redirect
+from django.urls import reverse
 # login_required, LoginRequiredMixin (class based view)
+
+
+class BreadcrumbMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['breadcrumbs'] = self.get_breadcrumbs()
+        return context
+
+    def get_breadcrumbs(self):
+        breadcrumbs = [{'title': 'Strona główna', 'url': reverse('app-home')}]
+
+        if isinstance(self, MachineDetailView):
+            machine = self.get_object()
+            breadcrumbs.append({'title': machine.name, 'url': reverse('machine-detail', kwargs={'pk': machine.pk})})
+        elif isinstance(self, MachineCreateView):
+            breadcrumbs.append({'title': 'Utwórz nową maszynę', 'url': reverse('machine-create')})
+        elif isinstance(self, MachineUpdateView):
+            machine = self.get_object()
+            breadcrumbs.append({'title': machine.name, 'url': reverse('machine-detail', kwargs={'pk': machine.pk})})
+            breadcrumbs.append({'title': 'Aktualizuj', 'url': reverse('machine-update', kwargs={'pk': machine.pk})})
+
+        return breadcrumbs
 
 
 @login_required
@@ -16,17 +43,17 @@ def home(request):
     return render(request, 'machines/machine.html', {'title': 'Home'})
 
 
-class MachineListView(ListView):
+class MachineListView(BreadcrumbMixin, ListView):
     model = Machine
     template_name = 'machines/machines_list.html'
 
 
-class MachineDetailView(DetailView):
+class MachineDetailView(BreadcrumbMixin, DetailView):
     model = Machine
     template_name = 'machines/machine_detail.html'
 
 
-class MachineCreateView(LoginRequiredMixin, CreateView):
+class MachineCreateView(LoginRequiredMixin, BreadcrumbMixin, CreateView):
     model = Machine
     template_name = 'machines/machine_form.html'
     form_class = MachineForm
@@ -37,7 +64,7 @@ class MachineCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class MachineUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class MachineUpdateView(BreadcrumbMixin, LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Machine
     template_name = 'machines/machine_form.html'
     fields = ['name', 'description']
@@ -59,3 +86,53 @@ class MachineDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         machine = self.get_object()
         return self.request.user == machine.author
+
+
+def generate_csv(request):
+    machines = Machine.objects.all()
+    data = []
+
+    for machine in machines:
+        tools = machine.tools.all()
+        for tool in tools:
+            data.append({
+                'Nr narzędzia': tool.tool_nr,
+                'Promień': tool.radius,
+                'Długość całkowita': tool.total_length,
+                'Długość poza oprawką': tool.outside_holder,
+                'Maszyna': machine.name,
+                'Typ oprawki': tool.holder.get_holder_type_display(),
+                'Typ freza': tool.tool.get_tool_type_display(),
+            })
+
+    df = pd.DataFrame(data)
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="data-csv.csv"'
+    df.to_csv(response, index=False)
+    return response
+
+
+# def generate_pdf(request):
+#     machines = Machine.objects.all()
+#     data = []
+#     for machine in machines:
+#         tools = machine.tools.all()
+#         for tool in tools:
+#             data.append({
+#                 'Nr narzędzia': tool.tool_nr,
+#                 'Promień': tool.radius,
+#                 'Długość całkowita': tool.total_length,
+#                 'Długość poza oprawką': tool.outside_holder,
+#                 'Maszyna': machine.name,
+#                 'Typ oprawki': tool.holder.get_holder_type_display(),
+#                 'Typ freza': tool.tool.get_tool_type_display(),
+#             })
+#
+#     context = {'data': data}
+#     html_string = render_to_string('machine_detail.html', context)
+#     html = HTML(string=html_string)
+#     pdf_file = html.write_pdf()
+#     response = HttpResponse(pdf_file, content_type='application/pdf')
+#     response['Content-Disposition'] = 'attachment; filename="data-pdf.pdf"'
+#     return response
+
