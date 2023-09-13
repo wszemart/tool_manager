a7b5b8f2229eba0be6433dcd8e3b0cd2c0e11375
wszemart <wszeborska@wp.pl>
2023-09-13 20:28:29 +0200

    Add first test case

---
+++ tool_manager/machines/sprawdzam czy tu działa.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import home, MachineListView, MachineDetailView, MachineCreateView, MachineUpdateView, MachineDeleteView
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_app_home_is_resolved(self):
+        url = reverse('app-home')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func, home)
+
+    def test_machine_detail_is_resolved(self):
+        url = reverse('machine-detail')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func.view_class, MachineDetailView)
