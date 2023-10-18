09d84e5a49c99d760d162e6751c02b6db453c24f
wszemart <wszeborska@wp.pl>
2023-10-18 19:43:37 +0200

    Fixed bugs in tests for the 'notifications' application.

--- tool_manager/users/apps.py
+++ tool_manager/users/apps.py
@@ -3,7 +3,6 @@ from django.dispatch import receiver
 from django.db.models.signals import post_migrate
 
 
-
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
