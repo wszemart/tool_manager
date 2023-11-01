2e3a740eefa399a6e8ddf0e09b8f1739efab6cee
wszemart <wszeborska@wp.pl>
2023-11-01 13:13:54 +0100

    Add help_text to models in machines app

--- tool_manager/machines/models.py
+++ tool_manager/machines/models.py
@@ -4,9 +4,11 @@ from django.urls import reverse
 
 
 class Machine(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=100, help_text="Machine name")
+    description = models.TextField(help_text="Machine description")
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True, help_text="Author of the machine"
+    )
 
     def get_absolute_url(self):
         return reverse("machine-detail", kwargs={"pk": self.pk})
