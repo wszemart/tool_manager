0667b99c440b26c34df4cfd406b6c8441b65ef18
wszemart <wszeborska@wp.pl>
2023-11-01 13:14:47 +0100

    Add help_text to models in notifications app

--- tool_manager/notifications/models.py
+++ tool_manager/notifications/models.py
@@ -5,14 +5,14 @@ from tools_assembly.models import UserComment
 
 
 class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_comment = models.ForeignKey(UserComment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Author of the notification")
+    user_comment = models.ForeignKey(UserComment, on_delete=models.CASCADE, help_text="User comment")
     message = models.TextField(default="New comment!")
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False, help_text="Notification status")
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Comment creation date")
 
 
 class UserNotification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    notification = models.ForeignKey(Notification, on_delete=models.CASCADE)
-    is_read = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Author of the user notification")
+    notification = models.ForeignKey(Notification, on_delete=models.CASCADE, help_text="Notification sent to users")
+    is_read = models.BooleanField(default=False, help_text="User notification status")
