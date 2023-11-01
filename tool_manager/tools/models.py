3c5e239974518f184c81c4fe8fb93735ad9682e2
wszemart <wszeborska@wp.pl>
2023-11-01 13:15:51 +0100

    Add help_text to models in tools app

--- tool_manager/tools/models.py
+++ tool_manager/tools/models.py
@@ -14,18 +14,32 @@ class Tool(models.Model):
 
     tool_type_dict = dict(tool_type_choices)
 
-    tool_type = models.CharField(max_length=100, choices=tool_type_choices)
-    catalog_nr = models.CharField(max_length=100)
-    R = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    L1 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    D1 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    L2 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    D2 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    L3 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    D3 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    L4 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    D4 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=None)
+    tool_type = models.CharField(max_length=100, choices=tool_type_choices, help_text="Tool type")
+    catalog_nr = models.CharField(max_length=100, help_text="Tool catalog number")
+    R = models.DecimalField(
+        max_digits=10, decimal_places=3, blank=True, null=True, help_text="Corner radius of a end- and bull- mill"
+    )
+    L1 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True, help_text="Blade length")
+    D1 = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True, help_text="Blade diameter")
+    L2 = models.DecimalField(
+        max_digits=10, decimal_places=3, blank=True, null=True, help_text="Length of the first shaft segment"
+    )
+    D2 = models.DecimalField(
+        max_digits=10, decimal_places=3, blank=True, null=True, help_text="Diameter of the first shaft segment"
+    )
+    L3 = models.DecimalField(
+        max_digits=10, decimal_places=3, blank=True, null=True, help_text="Length of the second shaft segment"
+    )
+    D3 = models.DecimalField(
+        max_digits=10, decimal_places=3, blank=True, null=True, help_text="Diameter of the second shaft segment"
+    )
+    L4 = models.DecimalField(
+        max_digits=10, decimal_places=3, blank=True, null=True, help_text="Length of the third shaft segment"
+    )
+    D4 = models.DecimalField(
+        max_digits=10, decimal_places=3, blank=True, null=True, help_text="Diameter of the third shaft segment"
+    )
+    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=None, help_text="Author of the tool")
 
     def __str__(self):
         return self.tool_type_dict.get(self.tool_type, "Unknown tool type")
