188d1b9357add60326f608e6480d9c9ba14a219d
wszemart <wszeborska@wp.pl>
2023-11-01 13:24:34 +0100

    Update URL path due to changes in user application methods.

--- tool_manager/tool_manager/urls.py
+++ tool_manager/tool_manager/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
-from users.views import logout, profile, register
+from users.views import LogoutUser, NewUserRegister, UserProfile
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -29,12 +29,12 @@ urlpatterns = [
     path("", include("tools.urls")),
     path("", include("holders.urls")),
     path("", include("notifications.urls")),
-    path("register/", register, name="register"),
+    path("register/", NewUserRegister.as_view(), name="register"),
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="users/login.html"),
         name="login",
     ),
-    path("logout/", logout, name="logout"),
-    path("profile/", profile, name="profile"),
+    path("logout/", LogoutUser.as_view(), name="logout"),
+    path("profile/", UserProfile.as_view, name="profile"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
