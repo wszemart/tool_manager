b4955118640f9adce2d95328752b60c8e7d9a088
wszemart <wszeborska@wp.pl>
2023-12-11 20:55:22 +0100

    Add host: 18.135.66.122

--- base/settings.py
+++ base/settings.py
@@ -168,14 +168,17 @@ STATIC_URL = "/static/"
 #     os.path.join(BASE_DIR, "static/"),
 # ]
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
-STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
+# AWS_ACCESS_KEY_ID = "AKIA3CEBUZTS47V6AQ5K"
+# AWS_SECRET_ACCESS_KEY = "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"
+# AWS_STORAGE_BUCKET_NAME = "codepipeline-eu-west-2-233135851116"
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
-
+AWS_S3_REGION_NAME = "eu-west-2"
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = "/media/"
